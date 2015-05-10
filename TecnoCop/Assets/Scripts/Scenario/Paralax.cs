a668243c0fc7f93cae2353050411b8717bf8bdaf
joaogameprog <joaogameprog@gmail.com>
2015-05-10 17:07:38 -0300

    Alterações nos tiles

--- TecnoCop/Assets/Scripts/Scenario/Paralax.cs
+++ TecnoCop/Assets/Scripts/Scenario/Paralax.cs
@@ -9,7 +9,8 @@ namespace TecnoCop{
 			// Update is called once per frame
 			void Update () {
 				Vector3 cp = Camera.main.transform.position;
-				GetComponent<Renderer>().material.SetTextureOffset("_MainTex",new Vector2(cp.x*intesisty.x - transform.position.x*0.0625f,-(transform.position.y-cp.y)*0.0625f +0.45f));
+				//GetComponent<Renderer>().material.SetTextureOffset("_MainTex",new Vector2(cp.x*intesisty.x - transform.position.x*0.0625f,-(transform.position.y-cp.y)*0.0625f +0.45f));
+				GetComponent<Renderer>().material.SetTextureOffset("_MainTex",new Vector2(cp.x*intesisty.x - transform.position.x *0.625f,-(transform.position.y-cp.y)*0.625f +0.45f));
 			}
 		}
 	}
