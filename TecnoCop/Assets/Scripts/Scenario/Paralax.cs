e327e502c2f386d95813ba10d08dd58a5464fb9d
Alexandre Petrassi <alexandrepetrassi@gmail.com>
2015-05-10 17:44:20 -0300

    Correção do parallax

--- TecnoCop/Assets/Scripts/Scenario/Paralax.cs
+++ TecnoCop/Assets/Scripts/Scenario/Paralax.cs
@@ -10,7 +10,7 @@ namespace TecnoCop{
 			void Update () {
 				Vector3 cp = Camera.main.transform.position;
 				//GetComponent<Renderer>().material.SetTextureOffset("_MainTex",new Vector2(cp.x*intesisty.x - transform.position.x*0.0625f,-(transform.position.y-cp.y)*0.0625f +0.45f));
-				GetComponent<Renderer>().material.SetTextureOffset("_MainTex",new Vector2(cp.x*intesisty.x - transform.position.x *0.625f,-(transform.position.y-cp.y)*0.625f +0.45f));
+				GetComponent<Renderer>().material.SetTextureOffset("_MainTex",new Vector2(cp.x*intesisty.x + transform.position.x *0.0625f,(transform.position.y-cp.y)*0.0625f +0.45f));
 			}
 		}
 	}
