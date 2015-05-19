2637dd6447a5728e1ac2209382c68a5209f8ebd7
Alexandre Petrassi <alexandrepetrassi@gmail.com>
2015-05-19 05:27:45 -0300

    Comera Controller. A camera se limita apenas a área do cenário, sem mostrar o background preto.

---
+++ TecnoCop/Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using TecnoCop.PlayerControl;
+using TecnoCop.Collisions;
+
+public class CameraController : MonoBehaviour {
+
+	ScenarioManager scenarioManager;
+	// Use this for initialization
+	void Start () {
+		scenarioManager = Camera.main.GetComponent<ScenarioManager>();
+	}
+
+	public Rect cBounds; // Limites da camera
+
+	// Update is called once per frame
+	void LateUpdate () {
+		if(Player.player == null) return;
+		Vector3 t = Player.player.transform.position;
+		float x = Mathf.Clamp(t.x,cBounds.xMin,scenarioManager.map.width  - cBounds.width);
+		float y = Mathf.Clamp(t.y,cBounds.yMin,scenarioManager.map.height - cBounds.height);
+		transform.position = new Vector3(x,y,-10);
+	}
+}
--- TecnoCop/Assets/Scripts/SceneControl/GameOverController.cs
+++ TecnoCop/Assets/Scripts/SceneControl/GameOverController.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameOverController : MonoBehaviour {
 
 	float bornTime;
 	public float WaitTime;
+	public float fadeTime;
+	public Image gameOverImage;
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +16,8 @@ public class GameOverController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if(bornTime + fadeTime < Time.time)
+			gameOverImage.color = gameOverImage.color + new Color(0.025f,0.025f,0.025f);
 		if(bornTime + WaitTime < Time.time || Input.anyKeyDown){
 			Application.LoadLevel(0);
 		}
