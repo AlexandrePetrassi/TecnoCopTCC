a6276e204a9f0bee8a09138a8e299bb81d4d06a3
Alexandre Petrassi <alexandrepetrassi@gmail.com>
2015-05-21 09:45:53 -0300

    Música

---
+++ TecnoCop/Assets/Scripts/SceneControl/MusicSingleton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicSingleton : MonoBehaviour {
+
+	private static MusicSingleton instance;
+	
+	public static MusicSingleton GetInstance(){
+		return instance;
+	}
+	
+	void Awake() {
+		if (instance != null && instance != this) {
+			if(instance.GetComponent<AudioSource>().clip != GetComponent<AudioSource>().clip){
+				instance.GetComponent<AudioSource>().clip = GetComponent<AudioSource>().clip;
+				instance.GetComponent<AudioSource>().Play();
+			}
+			Destroy(this.gameObject);
+			return;
+		} else {
+			instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+}
